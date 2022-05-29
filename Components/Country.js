443efef9240ef18fc8cd520160c1bb5d5d095f1d
Thowhid31 <thowhidantor16@gmail.com>
2022-05-29 17:59:10 +0600

    added all countries with react native

---
+++ Components/Country.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+// import { ScrollView } from 'react-native-web'
+
+export default function Country() {
+  return (
+    <View>
+      <Text>Country{country.name.common}</Text>
+        
+    </View>
+  )
+}
