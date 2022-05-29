32d104c9132f22c09a14db0a146a5a51ea7e01a1
Thowhid31 <thowhidantor16@gmail.com>
2022-05-29 18:15:21 +0600

    added countrie's photos and name

--- Components/Country.js
+++ Components/Country.js
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
-// import { ScrollView } from 'react-native-web'
 
-export default function Country() {
+export default function Country({country}) {
   return (
     <View>
-      <Text>Country{country.name.common}</Text>
-        
+      <Text style={{fontSize:20}}>Country: {country.name.common}</Text>
+      <Image
+      source={{
+          uri: country.flags.png
+      }}
+      style={{width: 200, height: 200}}
+      ></Image>
     </View>
   )
 }
